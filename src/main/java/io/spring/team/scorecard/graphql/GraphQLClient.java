c92f80dd7843fc91c09abb7546080feff1d65a6e
Rob Winch <rwinch@users.noreply.github.com>
2021-03-23 21:48:09 -0500

    Add some queries

--- src/main/java/io/spring/team/scorecard/graphql/GraphQLClient.java
+++ src/main/java/io/spring/team/scorecard/graphql/GraphQLClient.java
@@ -1,13 +1,16 @@
 package io.spring.team.scorecard.graphql;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Input;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
-import io.spring.team.scorecard.AssignableUsersQuery;
-import io.spring.team.scorecard.IssueCountQuery;
+import io.spring.team.scorecard.*;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -32,6 +35,98 @@ public class GraphQLClient {
 				.build();
 	}
 
+	public Mono<String> findIssueId() {
+		return Mono.create( sink -> this.apolloClient.query(new FindIssueIDQuery())
+				.enqueue(new ApolloCall.Callback<FindIssueIDQuery.Data>() {
+					@Override
+					public void onResponse(@NotNull Response<FindIssueIDQuery.Data> response) {
+						sink.success(response.getData().repository().issue().id());
+					}
+					@Override
+					public void onFailure(@NotNull ApolloException e) {
+						sink.error(e);
+					}
+				}));
+	}
+
+	public Mono<String> findRepositoryId(String owner, String name) {
+		return Mono.create( sink -> this.apolloClient.query(new FindRepositoryIdQuery(owner, name))
+				.enqueue(new ApolloCall.Callback<FindRepositoryIdQuery.Data>() {
+					@Override
+					public void onResponse(@NotNull Response<FindRepositoryIdQuery.Data> response) {
+						if (response.hasErrors()) {
+							sink.error(new RuntimeException(response.getErrors().stream().map(e -> e.getMessage()).collect(Collectors.joining(" "))));
+						} else {
+							sink.success(response.getData().repository().id());
+						}
+					}
+					@Override
+					public void onFailure(@NotNull ApolloException e) {
+						sink.error(e);
+					}
+				}));
+	}
+
+	public Mono<String> createPullRequest(String repositoryId, String baseRefName, String title, String body, String headRefName) {
+		return Mono.create(sink -> this.apolloClient.mutate(new CreatePullRequestMutation(baseRefName, Input.optional(body), headRefName, repositoryId, title))
+				.enqueue(new ApolloCall.Callback<CreatePullRequestMutation.Data>() {
+					@Override
+					public void onResponse(@NotNull Response<CreatePullRequestMutation.Data> response) {
+						if (response.hasErrors()) {
+							sink.error(new RuntimeException(response.getErrors().stream().map(e -> e.getMessage()).collect(Collectors.joining(" "))));
+						} else {
+							sink.success(response.getData().createPullRequest().pullRequest().id());
+						}
+					}
+
+					@Override
+					public void onFailure(@NotNull ApolloException e) {
+						sink.error(e);
+					}
+				}));
+	}
+
+
+	public Flux<Integer> findByTitle(String repositoryId, String title) {
+		return Flux.create(sink -> this.apolloClient.query(new FindPullRequestByTitleQuery("'Update org.springframework 5.2.0' in:title"))
+				.enqueue(new ApolloCall.Callback<FindPullRequestByTitleQuery.Data>() {
+					@Override
+					public void onResponse(@NotNull Response<FindPullRequestByTitleQuery.Data> response) {
+						if (response.hasErrors()) {
+							sink.error(new RuntimeException(response.getErrors().stream().map(e -> e.getMessage()).collect(Collectors.joining(" "))));
+						} else {
+							List<FindPullRequestByTitleQuery.AsPullRequest> issues = response.getData().search().nodes().stream().map(FindPullRequestByTitleQuery.AsPullRequest.class::cast).collect(Collectors.toList());
+							System.out.println(issues);
+							sink.next(response.getData().search().issueCount());
+							sink.complete();
+						}
+					}
+
+					@Override
+					public void onFailure(@NotNull ApolloException e) {
+						sink.error(e);
+					}
+				}));
+	}
+
+	public Mono<String> enablePullRequestAutoMerge(String pullRequestId) {
+		return Mono.create(sink -> this.apolloClient.mutate(new EnablePullRequestAutoMergeMutation(pullRequestId))
+				.enqueue(new ApolloCall.Callback<EnablePullRequestAutoMergeMutation.Data>() {
+					@Override
+					public void onResponse(@NotNull Response<EnablePullRequestAutoMergeMutation.Data> response) {
+						if (response.hasErrors()) {
+							sink.error(new RuntimeException(response.getErrors().stream().map(e -> e.getMessage()).collect(Collectors.joining(" "))));
+						} else {
+							sink.success(String.valueOf(response.getData().enablePullRequestAutoMerge().pullRequest().autoMergeRequest().enabledAt()));
+						}
+					}
+
+					@Override
+					public void onFailure(@NotNull ApolloException e) {
+						sink.error(e);
+					}
+				}));
+	}
 
 	public Mono<Integer> searchNumberOfIssuesAndPRs(String searchQuery) {
 		logger.debug("query: " + searchQuery);
